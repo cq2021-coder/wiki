3d0396d962c045ba0d065b6fa2d7a68f813add94
cq2021 <2972084238@qq.com>
2021-05-04 20:53:56 +0800

    3-5 增加demo表列表查询

---
+++ src/main/java/com/cq/wiki/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.cq.wiki.service;
+
+import com.cq.wiki.domain.Demo;
+import com.cq.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+
+}
