3fac23d764d2bb61347b7a8bb91d4fe89304f8f4
cq2021 <2972084238@qq.com>
2021-05-16 23:22:51 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/cq/wiki/service/EbookService.java
+++ src/main/java/com/cq/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.cq.wiki.service;
 
 import com.cq.wiki.domain.Ebook;
+import com.cq.wiki.domain.EbookExample;
 import com.cq.wiki.mapper.EbookMapper;
+import com.cq.wiki.req.EbookReq;
+import com.cq.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +18,18 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
-    }
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
+    }
 }
