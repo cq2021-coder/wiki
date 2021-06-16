c8b9906746c84d4d5d60a11652cbaf1c874413fa
cq2021 <2972084238@qq.com>
2021-06-16 18:25:39 +0800

    7-2 分类表设计与持久层代码生成 基本的无限级数型表结构设计：id+parent

---
+++ src/main/java/com/cq/wiki/mapper/CategoryMapper.java
@@ -0,0 +1,31 @@
+package com.cq.wiki.mapper;
+
+import com.cq.wiki.domain.Category;
+import com.cq.wiki.domain.CategoryExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CategoryMapper {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
