1f523b6d29b0f09976a427d1f4a1612ab965ab01
cq2021 <2972084238@qq.com>
2021-06-20 16:19:55 +0800

    8-11 增加保存文档成功提示 编辑和新增的时候，先清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -189,6 +189,7 @@ export default defineComponent({
         const data = response.data; // data = commonResp
         if (data.success) {
           modalVisible.value = false;
+          message.success("保存成功！")
 
           // 重新加载列表
           handleQuery();
@@ -280,6 +281,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = (record: any) => {
+      //清空富文本框
+      editor.txt.html("")
       modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -296,6 +299,8 @@ export default defineComponent({
      * 新增
      */
     const add = () => {
+      //清空富文本框
+      editor.txt.html("")
       modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
