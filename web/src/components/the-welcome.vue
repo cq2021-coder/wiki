3df1b4f2e16f04cdc42d493cfd7f556101447199
cq2021 <2972084238@qq.com>
2021-06-30 18:37:57 +0800

    11-7 集成echarts_5.0.2

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -224,13 +224,6 @@
   });
 </script>
 <style scoped>
-  .tip {
-    padding: 10px 5px;
-    margin-bottom: 20px;
-    border: 1px solid transparent;
-    background: linear-gradient(white,white) padding-box,repeating-linear-gradient(-45deg, black 0, black 25%, white 0, white 50%) 0/.6em .6em;
-    animation:ants 12s linear infinite;
-  }
   .tip b{
     color: red;
   }
