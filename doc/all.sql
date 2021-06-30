d31a150bb1eb56d5259b4d9529a4dfd853f244c2
cq2021 <2972084238@qq.com>
2021-06-30 17:42:18 +0800

    11-4 电子书快照表增加唯一键，一个电子书一天只能有一条记录

--- doc/all.sql
+++ doc/all.sql
@@ -109,6 +109,7 @@ create table `token`(
 
 update doc set view_count = view_count + 1 where id = 1;
 
+-- 电子书快照表
 drop table if exists `ebook_snapshot`;
 create table `ebook_snapshot`
 (
@@ -119,5 +120,6 @@ create table `ebook_snapshot`
     `vote_count` int not null default 0 comment '点赞数',
     `view_increase` int not null default 0 comment '阅读增长',
     `vote_increase` int not null default 0 comment '点赞增长',
-    primary key (`id`)
+    primary key (`id`),
+    unique key `ebook_id_date_unique` (`ebook_id` , `date`)
 )engine=innodb default charset=utf8mb4 comment='电子书快照表';
