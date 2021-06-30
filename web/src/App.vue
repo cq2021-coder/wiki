c9b92750869e351921e6c785a777afc837188d19
cq2021 <2972084238@qq.com>
2021-06-30 19:14:42 +0800

    网站优化

--- web/src/App.vue
+++ web/src/App.vue
@@ -23,13 +23,6 @@ export default defineComponent({
 </script>
 
 <style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
 
 .ant-row-rtl #components-layout-demo-top-side-2 .logo {
   float: right;
--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -325,7 +325,7 @@ export default defineComponent({
         ebookId: route.query.ebookId
       };
 
-      treeSelectData.value = Tool.copy(level1.value);
+      treeSelectData.value = Tool.copy(level1.value) || [];
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
